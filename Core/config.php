503eaf88340c675d386673a2543c2c9a06ee989a
Petar <marincicpetar@gmail.com>
2024-10-13 22:19:24 +0200

    added database config

---
+++ Core/config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core;
+
+
+return [
+   "database" => [
+      "host" => "localhost",
+      "port" => 3306,
+      "dbname" => "catalog",
+      "charset" => "utf8mb4"
+   ]
+];
