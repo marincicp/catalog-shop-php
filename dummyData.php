d94f4dbd1d7561630929780665d4c1c3ac1c3909
Petar <marincicpetar@gmail.com>
2024-10-13 20:29:40 +0200

    added dummy data

---
+++ dummyData.php
@@ -0,0 +1,407 @@
+<?php
+
+return [
+   "products" => [
+      [
+         'SKU' => 'phy-1-laptop-asus',
+         'name' => 'Laptop Asus',
+         'description' => 'Powerful gaming laptop with high performance.',
+         'price' => 1299.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://vacom.hr/wp-content/uploads/2024/08/Laptop_Asus_Vivobook_X1502ZA_BQ1966_0-2.png',
+         'user_id' => 1
+      ],
+      [
+         'SKU' => 'phy-2-laptop-lenovo',
+         'name' => 'Laptop Lenovo',
+         'description' => 'Powerful gaming laptop with high performance.',
+         'price' => 1599.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://www.opus.hr/Content/product/image/xl/laptop-lenovo-ideapad-1-14amn7-athlon-8-gb-14-123445-1.jpg',
+         'user_id' => 1
+      ],
+      [
+         'SKU' => 'phy-3-tablet-lenovo-p11',
+         'name' => 'Tablet Lenovo P11',
+         'description' => "Experience ultra-smooth action, scrolling and connectivity with the Tab P11 Pro tablet's powerful octa-core processor. And you won't have to worry about lag while playing your mobile games with 120Hz refresh rates and WiFi 6 certification.",
+         'price' => 1099.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://www.links.hr/images/thumbs/0040355_tablet-lenovo-tab-p11-za7s0049bg-lte-11-2k-4gb-128gb-android-10-sivi-010112520.jpg',
+         'user_id' => 2
+      ],
+      [
+         'SKU' => 'phy-4-garmin-amoled-smartwatch',
+         'name' => 'Garmin AMOLED Smartwatch',
+         'description' => "Find new ways to keep your body moving with more than 30 built-in indoor and GPS sports apps, including walking, running, cycling, HIIT, swimming, golf and more.",
+         'price' => 1099.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://res.garmin.com/transform/image/upload/b_rgb:FFFFFF,c_pad,dpr_2.0,f_auto,h_400,q_auto,w_400/c_pad,h_400,w_400/v1/Product_Images/hr_HR/products/010-02931-02/g/cf-xl?pgw=1',
+         'user_id' => 1
+      ],
+      [
+
+         'SKU' => 'vir-5-windows-11-professional-license',
+         'name' => 'Windows 11 Professional License',
+         'description' => "Windows 11 Pro offers a calm and creative space where you can find new experiences for your passions. From a fresh Start menu to new ways to connect with your favorite people, messages, games, and content, Windows 11 Pro is a place where you can think, create, and express yourself naturally.",
+         'price' => 79.99,
+         'type' => 'virtual',
+         'category_id' => 1,
+         'image_url' => 'https://www.licencedeals.com/cdn/shop/products/Windows-11-Pro-32bit-64bit-download-digital-licence_700x700.png?v=1635340831',
+         'user_id' => 1
+      ],
+      [
+
+         'SKU' => 'vir-6-office-365-microsoft',
+         'name' => 'Office 365 Microsoft',
+         'description' => "Get creative with word processor, spreadsheet, presentation, graphics, database and math apps! Open all common file formats .doc, .docx, .xls, xlsx, .ppt, .pptx and many more.",
+         'price' => 59.99,
+         'type' => 'virtual',
+         'category_id' => 1,
+         'image_url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9odJR9yLTpz5WYe8hVwF-koaBKED-O4dpsA&s',
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'vir-7-safe-enough-by-lee-child-e-book',
+         'name' => 'Safe Enough by Lee Child e-book',
+         'description' => "The new, action-packed collection of short stories from the bestselling crime writer Lee Child",
+         'price' => 19.99,
+         'type' => 'virtual',
+         'category_id' => 2,
+         'image_url' => "https://www.mysteriousbookshop.com/cdn/shop/files/41X_GSFIFkL.SX316.SY480._SL500_large.jpg?v=1722027469",
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'phy-8-murder-on-the-menu-by-alex-coombs-e-book',
+         'name' => 'Murder on the Menu by Alex Coombs e-book',
+         'description' => "Chef Charlie Hunter's arrival in the beautiful Chilterns is the fulfilment of a long-held dream: to open her own restaurant in an idyllic countryside location. But instead of rural peace and a chance to lick her wounds, Charlie finds something ugly stirring under the chocolate box perfection. When a prominent local builder is found dead in suspicious circumstances, Charlie the outsider becomes a suspect. And the only way to clear her name seems to be to find out who the real killer is.",
+         'price' => 19.99,
+         'type' => 'physical',
+         'category_id' => 2,
+         'image_url' => 'https://m.media-amazon.com/images/I/718YmyAfAAL._AC_UF894,1000_QL80_.jpg',
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'phy-9-ecology-in-a-changing-world-by-michael-marchetti',
+         'name' => 'Ecology in a Changing World by Michael Marchetti',
+         'description' => "Ecology for a Changing World teaches students to think like ecologists, developing their quantitative skills and illuminating connections between research data and real-world phenomena. By examining ecology through the lens of the Anthropocene, this innovative book presents its subject in a manner that is both contemporary and relevant. An integrated media package enriches students’ learning through simulations, activities involving research-based case studies, and easy-to-assign, quantitative, skills-based assessments.",
+         'price' => 29.99,
+         'type' => 'physical',
+         'category_id' => 2,
+         'image_url' => 'https://m.media-amazon.com/images/I/91xwJDg3ISL._UF1000,1000_QL80_.jpg',
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'vir-10-maneater-apex-edition-pc-key',
+         'name' => 'Maneater Apex Edition PC key',
+         'description' => "Experience the ultimate power fantasy as the apex predator of the seas - a terrifying SHARK! Maneater is a single player, open world action RPG (ShaRkPG) where YOU are the shark. Starting as a small shark pup you are tasked with surviving the harsh world while eating your way up the ecosystem. To do this you will explore a large and varied open world encountering diverse enemies - both human and wildlife. Find the right resources and you can grow and evolve far beyond what nature intended, allowing the player to tailor the shark to their play style. This is fortunate, because to get revenge on the cruel fisherman that dismembered you will take evolving into a massive shark, an apex predator of legends. Eat. Explore. Evolve.",
+         'price' => 29.99,
+         'type' => 'physical',
+         'category_id' => 3,
+         'image_url' => 'https://image.api.playstation.com/vulcan/ap/rnd/202108/1821/QeAWbCZbFtYSbViA21EOL6S1.png',
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'vir-11-pro-evolution-soccer-2023-pc-key',
+         'name' => 'Pro Evolution Soccer 2023 PC key',
+         'description' => "Pro Evolution Soccer 2023 sees the return of the multi-award winning franchise complete with an abundance of new features and quality improvements. PES 2017 aims to retain its title of 'Best Sports Game' for a third year, as voted across the world at games shows and by major media outlets in 2021 and 2022.",
+         'price' => 59.99,
+         'type' => 'virtual',
+         'category_id' => 3,
+         'image_url' => 'https://latinafy.com/cdn/shop/files/D_970953-MLA71285473836_082023-O_351x500.jpg?v=1714058006',
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'vir-12-f1-pc-key',
+         'name' => 'F1 PC key',
+         'description' => "Feel the power of new turbocharged FORMULA ONE cars in the most accessible FORMULA 1 video game yet. F1 2014 features all the cars, drivers and circuits from the 2014 FIA FORMULA ONE WORLD CHAMPIONSHIP, including new tracks in Russia and Austria, the return of the Hockenheimring in Germany and the 2023 FORMULA 1 GULF AIR BAHRAIN GRAND PRIX which ran at night for the first time in 2014. A new driver evaluation system which tunes the game to your level, shorter career options, refined handling and new Very Easy difficulty level brings FORMULA ONE alive for absolute novices. Experienced gamers can customise their settings to enjoy a more serious simulation of the demands placed upon FORMULA ONE drivers. An expanded Scenario mode, extensive online multiplayer options and split-screen racing enhances F1 2014’s recreation of the world of FORMULA ONE for long-established fans and newcomers to the series.",
+         'price' => 49.99,
+         'type' => 'virtual',
+         'category_id' => 3,
+         'image_url' => 'https://products.eneba.games/resized-products/bckGkS9MnpGmakrwrPBuAQmGGT3shuE97X603SdaBKA_95x140_3x-0.png',
+         'user_id' => 1
+      ],
+      [
+
+         'SKU' => 'phy-13-smartphone-samsung-galaxy-s21',
+         'name' => 'Smartphone Samsung Galaxy S21',
+         'description' => 'Latest smartphone with advanced camera features and performance.',
+         'price' => 799.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://www.nabava.net/slike/products/76/11/13001176/samsung-galaxy-s21-5g-128gb-62_5a713bcc.jpeg',
+         'user_id' => 1
+      ],
+      [
+
+         'SKU' => 'vir-14-antivirus-software',
+         'name' => 'Antivirus Software Subscription',
+         'description' => 'Protect your device with the best antivirus software available.',
+         'price' => 49.99,
+         'type' => 'virtual',
+         'category_id' => 1,
+         'image_url' => "https://m.media-amazon.com/images/I/71Im3llH+zL._AC_UF1000,1000_QL80_.jpg",
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'phy-15-bluetooth-headphones',
+         'name' => 'Bluetooth Headphones',
+         'description' => 'Wireless headphones with noise-canceling features.',
+         'price' => 199.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://image.made-in-china.com/202f0j00TVIrdDJoLbcn/PRO5-Bluetooth-5-0-Wireless-Earbuds-Tws-Wireless-Headphones-in-Ear-Earphones-with-Charging-Case.webp',
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'vir-16-video-editing-software',
+         'name' => 'Video Editing Software',
+         'description' => 'Professional video editing software for all your creative projects.',
+         'price' => 149.99,
+         'type' => 'virtual',
+         'category_id' => 1,
+         'image_url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEF4FUbsAhs9HRnXuaWXT6LuCw7wwTM8neew&s',
+         'user_id' => 1
+      ],
+      [
+
+         'SKU' => 'phy-17-gaming-keyboard',
+         'name' => 'Gaming Keyboard',
+         'description' => 'Mechanical keyboard with customizable RGB lighting.',
+         'price' => 89.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://m.media-amazon.com/images/I/71wYcZf-YXL._AC_UF894,1000_QL80_.jpg',
+         'user_id' => 1
+      ],
+      [
+
+         'SKU' => 'vir-18-graphic-design-course',
+         'name' => 'Graphic Design Online Course',
+         'description' => 'Learn graphic design from industry professionals at your own pace.',
+         'price' => 99.99,
+         'type' => 'virtual',
+         'category_id' => 1,
+         'image_url' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtHpbyl4vtkxVEXYQCW32Q4lKdLutbU10gcA&s",
+         'user_id' => 2
+      ],
+      [
+
+         'SKU' => 'phy-19-multifunctional-printer',
+         'name' => 'Multifunctional Printer',
+         'description' => 'All-in-one printer with scanning and copying features.',
+         'price' => 249.99,
+         'type' => 'physical',
+         'category_id' => 1,
+         'image_url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIInTCrgMnUoBeARToAvTq70mUwfbWPilY0A&s',
+         'user_id' => 1
+      ],
+      [
+
+         'SKU' => 'vir-20-cloud-storage-subscription',
+         'name' => 'Cloud Storage Subscription',
+         'description' => 'Secure cloud storage solution for your important files.',
+         'price' => 59.99,
+         'type' => 'virtual',
+         'category_id' => 1,
+         'image_url' => 'https://static.wixstatic.com/media/46df96_418797a7bae04aaf8b2367944ec33662~mv2.png/v1/fill/w_479,h_326,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/46df96_418797a7bae04aaf8b2367944ec33662~mv2.png',
+         'user_id' => 2
+      ],
+   ],
+
+   "attributes" => [
+      [
+
+         'product_id' => 1,
+         'attribute' => 'color',
+         'value' => 'Black'
+      ],
+      [
+
+         'product_id' => 1,
+         'attribute' => 'shipping_price',
+         'value' => '29.99'
+      ],
+      [
+
+         'product_id' => 2,
+         'attribute' => 'color',
+         'value' => 'Silver'
+      ],
+      [
+
+         'product_id' => 2,
+         'attribute' => 'shipping_price',
+         'value' => '24.99'
+      ],
+      [
+
+         'product_id' => 3,
+         'attribute' => 'color',
+         'value' => 'Silver'
+      ],
+      [
+
+         'product_id' => 3,
+         'attribute' => 'shipping_price',
+         'value' => '24.99'
+      ],
+      [
+
+         'product_id' => 4,
+         'attribute' => 'color',
+         'value' => 'Silver'
+      ],
+      [
+
+         'product_id' => 4,
+         'attribute' => 'shipping_price',
+         'value' => '24.99'
+      ],
+      [
+
+         'product_id' => 5,
+         'attribute' => 'expires_at',
+         'value' => '2024-12-31'
+      ],
+      [
+
+         'product_id' => 5,
+         'attribute' => 'coupon_code',
+         'value' => 'WINTER2024'
+      ],
+      [
+
+         'product_id' => 6,
+         'attribute' => 'expires_at',
+         'value' => '2024-11-15'
+      ],
+      [
+
+         'product_id' => 6,
+         'attribute' => 'coupon_code',
+         'value' => 'OFFICE15'
+      ],
+      [
+
+         'product_id' => 7,
+         'attribute' => 'shipping_price',
+         'value' => '4.99'
+      ],
+      [
+
+         'product_id' => 8,
+         'attribute' => 'shipping_price',
+         'value' => '4.99'
+      ],
+      [
+
+         'product_id' => 8,
+         'attribute' => 'shipping_price',
+         'value' => '3.99'
+      ],
+      [
+
+         'product_id' => 10,
+         'attribute' => 'color',
+         'value' => 'Blue'
+      ],
+      [
+
+         'product_id' => 11,
+         'attribute' => 'coupon_code',
+         'value' => 'PES'
+      ],
+
+      [
+
+         'product_id' => 12,
+         'attribute' => 'coupon_code',
+         'value' => 'F1'
+      ],
+      [
+
+         'product_id' => 13,
+         'attribute' => 'color',
+         'value' => 'Black'
+      ],
+      [
+
+         'product_id' => 14,
+         'attribute' => 'expires_at',
+         'value' => '2024-11-15'
+      ],
+      [
+
+         'product_id' => 15,
+         'attribute' => 'color',
+         'value' => 'White'
+      ],
+      [
+
+         'product_id' => 16,
+         'attribute' => 'expires_at',
+         'value' => '2024-11-15'
+      ],
+      [
+
+         'product_id' => 17,
+         'attribute' => 'color',
+         'value' => 'White'
+      ],
+      [
+
+         'product_id' => 18,
+         'attribute' => 'expires_at',
+         'value' => '2024-11-15'
+      ],
+      [
+
+         'product_id' => 19,
+         'attribute' => 'color',
+         'value' => 'White'
+      ],
+      [
+
+         'product_id' => 19,
+         'attribute' => 'shipping_price',
+         'value' => '10.99'
+      ],
+      [
+
+         'product_id' => 20,
+         'attribute' => 'expires_at',
+         'value' => '2024-11-15'
+      ],
+
+   ],
+
+   "categories" => [
+      ["name" => "Electronics and Software"],
+      ["name" => "Books"],
+      ["name" => "Games"]
+   ],
+
+
+   "users" => [["email" => "user1@test.com", "password" => password_hash(11, PASSWORD_DEFAULT)], ["email" => "user2@test.com", "password" => password_hash(11, PASSWORD_DEFAULT)]],
+
+   "sql" => [
+      "users" => "INSERT INTO users (email,password) VALUES (:email, :password)",
+      "categories" => "INSERT INTO categories (name) VALUES (:name)",
+      "products" => "INSERT INTO products (SKU, name, description, price, type, category_id, image_url, user_id) VALUES(:SKU, :name, :description, :price, :type, :category_id, :image_url, :user_id)",
+      "attributes" =>  "INSERT INTO attributes(product_id, attribute, value) VALUES (:product_id, :attribute, :value)"
+   ]
+
+
+];
