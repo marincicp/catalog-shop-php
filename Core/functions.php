085946ec3d18ad13183550379ed4fe315db408d4
Petar <marincicpetar@gmail.com>
2024-10-14 14:51:23 +0200

    added ProductController

--- Core/functions.php
+++ Core/functions.php
@@ -18,3 +18,20 @@ function abort($code = 404, $message = "The requested route could not be found")
    echo json_encode(["error" => $message]);
    exit();
 }
+
+
+function sendJsonRes($data, $code = 200)
+{
+   http_response_code($code);
+   header('Content-Type: application/json');
+   echo json_encode($data);
+   exit();
+}
+
+function formatRes($data)
+{
+   if ($data) {
+      return ["data" => $data, "code" => 200];
+   }
+   return ["error" => "No data found", "code" => 404];
+}
--- Http/Controllers/ProductController.php
+++ Http/Controllers/ProductController.php
@@ -1,16 +1,25 @@
 <?php
 
-
 namespace Http\Controllers;
 
+use Models\ProductModel;
 
+require "./Models/ProductModel.php";
 
 class ProductController
 {
 
-   public static function index($query)
+   public static function index()
    {
+      $products = ProductModel::get();
+      return sendJsonRes($products, $products["code"]);
+   }
 
-      echo "index";
+
+
+   public static function show($sku)
+   {
+      $product = ProductModel::find($sku);
+      return sendJsonRes($product, $product["code"]);
    }
 }
