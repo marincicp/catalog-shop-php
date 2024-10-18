6cc943b106258c2ff1938e5f4855b9c9b6cc3bd2
Petar <marincicpetar@gmail.com>
2024-10-18 12:14:14 +0200

    cors

--- index.php
+++ index.php
@@ -12,10 +12,19 @@ $router = new Router();
 $routes = require "routes.php";
 $config = require "Core/config.php";
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Access-Control-Allow-Methods: GET, POST,PUT,DELETE, OPTIONS");
+
 $parsedUri = parse_url($_SERVER["REQUEST_URI"]);
 $uri = $parsedUri["path"];
 $query = $parsedUri["query"] ?? "";
 $method = $_SERVER["REQUEST_METHOD"];
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+   http_response_code(200);
+   exit();
+}
 
 // TODO You should first create the database 'catalog' and
 // TODO then run the code below.
